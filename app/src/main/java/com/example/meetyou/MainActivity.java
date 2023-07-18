aa69a582b35d3ddbecd088ab5a84f1818ac2ac69
Bubka Gop <fajardofamas839@gmail.com>
2023-07-19 00:13:05 +0300

    first commit

---
+++ app/src/main/java/com/example/meetyou/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.meetyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.main));
+    }
+}
