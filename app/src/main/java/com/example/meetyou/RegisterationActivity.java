ef5d186ed71db759963c13ed92b0fa7fac012341
Bubka Gop <fajardofamas839@gmail.com>
2023-07-19 03:55:37 +0300

    Add email validation

--- app/src/main/java/com/example/meetyou/RegisterationActivity.java
+++ app/src/main/java/com/example/meetyou/RegisterationActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;;
 
 public class RegisterationActivity extends AppCompatActivity {
 
@@ -24,5 +27,28 @@ public class RegisterationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        AppCompatButton signUpButton = findViewById(R.id.sign_up_button);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailEditText = findViewById(R.id.mail_text);
+                String email = emailEditText.getText().toString().trim();
+
+                if (isValidEmail(email)) {
+                    //здесь будет код регистрации
+                    Toast.makeText(RegisterationActivity.this, "Успешная регистрация!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Нздесь будет кастомная нотифайка
+                    Toast.makeText(RegisterationActivity.this, "Некорректный формат электронной почты", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    private boolean isValidEmail(String email) {
+
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
 }
