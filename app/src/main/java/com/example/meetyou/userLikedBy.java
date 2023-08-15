e1d2df4a0010610130d3c9c9395aee709ff9feff
sokk0l <56171077+Sokk0l@users.noreply.github.com>
2023-08-15 22:42:36 +0300

    added nonmegalike.xml and megalike.xml

---
+++ app/src/main/java/com/example/meetyou/userLikedBy.java
@@ -0,0 +1,14 @@
+package com.example.meetyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userLikedBy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_liked_by);
+    }
+}
